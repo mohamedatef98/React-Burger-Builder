d6dbf418838db3129568edf07df8c98b63a7f659
Mohamed Atef <eng.mohamedatef98@gmail.com>
2019-05-21 23:57:44 +0200

    use burgerIngredient to build burger

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,8 +1,17 @@
 import React from 'react'
 
+import classes from './BurgerIngredient.module.css'
+
+import BurgerIngredient from '/components/Burger/BurgerIngredient/BurgerIngredient'
+
 const burger = props => {
     return (
-        <div>
+        <div className={classes['Burger']}>
+            <BurgerIngredient type="bread-top" />
+            <BurgerIngredient type="cheese" />
+            <BurgerIngredient type="meat" />
+            <BurgerIngredient type="bread-bottom" />
+
         </div>
     )
 
