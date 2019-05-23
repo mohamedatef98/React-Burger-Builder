64ba9ece8c2c4e01656f1e64891e1ebb3b79181b
Mohamed Atef <eng.mohamedatef98@gmail.com>
2019-05-23 20:00:09 +0200

    Video -- Outputting Multiple Build Controls

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react'
 
 import Burger from 'components/Burger/Burger'
+import BuildControls from 'components/Burger/BuildControls/BuildControls'
 
 class BurgerBuilder extends Component{
 
@@ -18,7 +19,7 @@ class BurgerBuilder extends Component{
         return (
             <>
                 <Burger ingredients={this.state.ingredients} />
-                <div>Burger Builder</div>
+                <BuildControls />
             </>
         )
     }
