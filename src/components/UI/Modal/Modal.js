efb296749cccfb905ba16fb4c732d87f20f84391
Mohamed Atef <eng.mohamedatef98@gmail.com>
2019-05-29 18:18:30 +0200

    Video -- Showing & Hiding the Modal (with Animation!)

--- src/components/UI/Modal/Modal.js
+++ src/components/UI/Modal/Modal.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import classnames from 'classnames/bind'
 
 import classes from './Modal.module.css'
 
+const cx = classnames.bind(classes)
+
 const modal = props => {
     return (
-        <div className={classes['Modal']}>
+        <div className={cx({ 'Modal': true, 'Open': props.show }) }>
             {props.children}
         </div>
     );
